63092377056b24a34604a825b66306323be635c1
obouhlel <oussama_bouhlel@outlook.fr>
2025-02-17 10:11:12 +0100

    Add files

--- src/describe.py
+++ src/describe.py
@@ -1,5 +1,12 @@
 import sys
 
+# First of all, take a look at the available data. look in what format it is presented, if
+# there are various types of data, the different ranges, and so on. It is important to make
+# an idea of your raw material before starting. The more you work on data - the more you
+# develop an intuition about how you will be able to use it.
+# In this part, Professor McGonagall asks you to produce a program called describe.[extension].
+# This program will take a dataset as a parameter.
+
 def describe():
     if len(sys.argv) != 2:
         print("Usage: need to give a dataset")
