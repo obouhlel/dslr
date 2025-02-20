042bf7a621a9639c27450ad36b3c0a8ebd5d216f
Bilel-Gh <bilelghandri@outlook.fr>
2025-02-20 02:07:35 +0100

    histogram version 0.5

--- src/histogram.py
+++ src/histogram.py
@@ -6,25 +6,8 @@ import pandas as pd
 
 from src.utils import ft_mean, ft_std, normalize_dataset, ft_max, ft_min
 
-
-def plot_single_course_histogram(dataset, course_name):
-    houses = dataset['Hogwarts House'].unique()
-    colors = ['red', 'blue', 'yellow', 'green']
-    plt.figure(figsize=(12, 8))
-
-    for i, house in enumerate(houses):
-        house_data = dataset[dataset['Hogwarts House'] == house][course_name].dropna()
-        plt.hist(house_data, bins=20, alpha=0.5, label=house, color=colors[i])
-
-    plt.title(f'Distribution des notes en {course_name} par maison', fontsize=16)
-    plt.xlabel('Value', fontsize=14)
-    plt.ylabel('Nombre d\'élèves', fontsize=14)
-    plt.legend()
-    plt.grid(alpha=0.3)
-    plt.show()
-
-def plot_comparative_histogram(original_dataset, normalized_dataset,
-                               course_name):
+def plot_comparative_histogram(original_dataset,
+                               course1, course2):
     houses = original_dataset['Hogwarts House'].unique()
     colors = ['red', 'blue', 'yellow', 'green']
 
@@ -33,25 +16,27 @@ def plot_comparative_histogram(original_dataset, normalized_dataset,
     for i, house in enumerate(houses):
         house_data = \
         original_dataset[original_dataset['Hogwarts House'] == house][
-            course_name].dropna()
+            course1].dropna()
         ax1.hist(house_data, bins=20, alpha=0.5, label=house,
                  color=colors[i])
 
-    ax1.set_title(f'Distribution originale - {course_name}', fontsize=16)
-    ax1.set_xlabel('Valeur originale', fontsize=14)
+    ax1.set_title(f'Distribution la plus homogène - {course1}',
+                  fontsize=16)
+    ax1.set_xlabel('Valeur', fontsize=14)
     ax1.set_ylabel('Nombre d\'élèves', fontsize=14)
     ax1.legend()
     ax1.grid(alpha=0.3)
 
     for i, house in enumerate(houses):
         house_data = \
-        normalized_dataset[normalized_dataset['Hogwarts House'] == house][
-            course_name].dropna()
+        original_dataset[original_dataset['Hogwarts House'] == house][
+            course2].dropna()
         ax2.hist(house_data, bins=20, alpha=0.5, label=house,
                  color=colors[i])
 
-    ax2.set_title(f'Distribution normalisée - {course_name}', fontsize=16)
-    ax2.set_xlabel('Valeur normalisée (z-score)', fontsize=14)
+    ax2.set_title(f'Distribution la moins homogène - {course2}',
+                  fontsize=16)
+    ax2.set_xlabel('Valeur', fontsize=14)
     ax2.set_ylabel('Nombre d\'élèves', fontsize=14)
     ax2.legend()
     ax2.grid(alpha=0.3)
@@ -72,23 +57,25 @@ def calculate_homogeneity(dataset, course_col):
                 'std': ft_std(house_data)
             }
         means = [stats[house]['mean'] for house in houses]
-        mean_range = ft_max(means) - ft_min(means) # coder son propre maxx et
-        # min
+        mean_range = ft_max(means) - ft_min(means)
         homogeneity_results[course] = {
             'stats': stats,
             'mean_range': mean_range
         }
     sorted_courses = sorted(homogeneity_results.items(),
-                            key=lambda x: x[1]['mean_range']) #verifier si
-    # sorted est autoriser
+                            key=lambda x: x[1]['mean_range'])
+    sorted_course_name = [course for course, data in sorted_courses]
 
     print("Cours classés du plus homogène au moins homogène:")
     for course, data in sorted_courses:
         print(f"{course}: écart entre moyennes = {data['mean_range']:.2f}")
         for house in houses:
             print(
-                f"  {house}: moyenne = {data['stats'][house]['mean']:.2f}, écart-type = {data['stats'][house]['std']:.2f}")
+                f"  {house}: moyenne = {data['stats'][house]['mean']:.2f}, "
+                f"écart-type = {data['stats'][house]['std']:.2f}")
         print()
+
+    return sorted_course_name
 
 
 def show_histogram():
@@ -97,9 +84,10 @@ def show_histogram():
                                            ).columns
     course_col = [num for num in numeric_col if num != 'Index']
     normalized_dataset = normalize_dataset(dataset, course_col)
-    calculate_homogeneity(normalized_dataset, course_col)
-    for course in course_col:
-        plot_comparative_histogram(dataset, normalized_dataset, course)
+    sorted_courses = calculate_homogeneity(normalized_dataset, course_col)
+
+    plot_comparative_histogram(dataset, sorted_courses[
+        0], sorted_courses[-1])
 
 if __name__ == "__main__":
     show_histogram()
